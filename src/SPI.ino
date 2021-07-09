8600cbc210bd1293390154bc35694868236794a6
hydrobenzene <86672580+hydrobenzene@users.noreply.github.com>
2021-07-08 22:25:15 -0600

    Add files via upload

---
+++ src/SPI.ino
@@ -0,0 +1,27 @@
+SYSTEM_THREAD(ENABLED);
+#include "blynk.h"
+#include "LIS3DH.h"
+
+LIS3DHSPI acrmtr(SPI, D3, WKP);
+void setup() {
+  pinMode(D3, INPUT);
+
+  Blynk.begin("{TOKEN}", IPAddress(000, 000, 000, 000), 0000);
+  Blynk.run();
+
+  Serial.begin(9600);
+
+  LIS3DHConfig cfg;
+	cfg.setAccelMode(LIS3DH::RATE_100_HZ);
+  Serial.println(acrmtr.setup(cfg))
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  LIS3DHSample sample;
+  acrmtr.getSample(instant);
+  Serial.println(sample.x + ", " + sample.y + ", " + sample.z)
+  Blynk.virtualWrite(V0, sample.x);
+  Blynk.virtualWrite(V1, sample.y);
+  Blynk.virtualWrite(V2, sample.z);
+}
